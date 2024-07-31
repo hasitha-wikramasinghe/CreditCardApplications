d686c3763dde07bb2ae1606d5ce9a1ecc043efc0
Hasitha Wikramasinghe <hasitha@corzent.com>
2024-07-31 16:24:07 +0530

    Add project files.

---
+++ CreditCardApplications/CreditCardApplication.cs
@@ -0,0 +1,12 @@
+﻿namespace CreditCardApplications
+{
+    public class CreditCardApplication
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public int Age { get; set; }
+        public decimal GrossAnnualIncome { get; set; }
+        public string FrequentFlyerNumber { get; set; } = string.Empty;
+    }
+}
---
+++ CreditCardApplications/CreditCardApplicationDecision.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreditCardApplications
+{
+    public enum CreditCardApplicationDecision
+    {
+        Unknown,
+        AutoAccepted,
+        AutoDeclined,
+        ReferredToHuman
+    }
+}
